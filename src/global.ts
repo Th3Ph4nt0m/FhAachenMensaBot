52d93a10de7308b06c2919e900aeb786b4d0789d
Henrik Steffens <development@th3ph4nt0m.de>
2023-11-28 09:21:36 +0100

    add KMAC and Südpark (api_id doen't exist yet)

--- src/global.ts
+++ src/global.ts
@@ -1,60 +1,72 @@
-import {Schema, model} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-export interface Canteen { 
-    name: string; 
-    api_id: number; 
-    canteen_id: number;
-} 
+export interface Canteen {
+  name: string;
+  api_id: number;
+  canteen_id: number;
+}
 
-export interface Meal { 
-    description: string; 
-    category: string; 
-    price: number;
-} 
+export interface Meal {
+  description: string;
+  category: string;
+  price: number;
+}
 
 export interface Menue {
-    meals?: Meal[];
-    sides?: string;
-    vegetables?: string; 
-    open?: boolean;
+  meals?: Meal[];
+  sides?: string;
+  vegetables?: string;
+  open?: boolean;
 
 }
 
-export let allCanteens: Canteen[] = [ 
-    {
-        name: "Mensa Academica",
-        api_id: 187,
-        canteen_id: 1,
-    },
-    {
-        name: "Mensa Vita",
-        api_id: 96,
-        canteen_id: 2,
-    },
-    {
-        name: "Mensa Ahorn",
-        api_id: 95,
-        canteen_id: 3,
-    },
-    {
-        name: "Mensa Bistro Tempelgraben",
-        api_id: 94,
-        canteen_id: 4,
-    },
-    {
-        name: "Mensa Eupener Straße",
-        api_id: 98,
-        canteen_id: 5,
-    },
-    {
-        name: "Mensa Bayernallee",
-        api_id: 97,
-        canteen_id: 6,
-    },
+export let allCanteens: Canteen[] = [
+  {
+    name: "Mensa Academica",
+    api_id: 187,
+    canteen_id: 1,
+  },
+  {
+    name: "Mensa Vita",
+    api_id: 96,
+    canteen_id: 2,
+  },
+  {
+    name: "Mensa Ahorn",
+    api_id: 95,
+    canteen_id: 3,
+  },
+  {
+    name: "Mensa Bistro Tempelgraben",
+    api_id: 94,
+    canteen_id: 4,
+  },
+  {
+    name: "Mensa Eupener Straße",
+    api_id: 98,
+    canteen_id: 5,
+  },
+  {
+    name: "Mensa Bayernallee",
+    api_id: 97,
+    canteen_id: 6,
+  },
+  {
+    name: "Mensa Südpark",
+    // TODO add api_id as soon as provided by the API
+    api_id: 0,
+    canteen_id: 7,
+  },
+  {
+    name: "Mensa KMAC",
+    // TODO add api_id as soon as provided by the API
+    api_id: 0,
+    canteen_id: 8,
+  },
 ];
 
 const userShema = new Schema({
-  chat_id: { 
+  chat_id: {
     type: String,
     required: true
   },
@@ -65,11 +77,11 @@ const userShema = new Schema({
     type: Date,
     default: Date.now
   },
-  canteen_id : {
+  canteen_id: {
     type: Number,
     default: 5,
     required: true
-  }, 
+  },
   time: {
     type: String,
     default: "09:30",
